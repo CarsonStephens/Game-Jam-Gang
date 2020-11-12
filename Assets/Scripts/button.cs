0f29fc8a7a2be2480ab773070d3681c35356d01d
Nick Santos <santos_nick@outlook.com>
2020-11-12 14:41:57 -0800

    Update

--- Assets/Scripts/button.cs
+++ Assets/Scripts/button.cs
@@ -33,20 +33,20 @@ public class button : MonoBehaviour
     {
         if (isTimed == true && buttonActive == true)
         {
-            if (timer <= 0)
-            {
-                buttonActive = false;
-                timer = timerDuration;
-                GetComponent<BoxCollider2D> ().enabled = true;
-                mySR.color = OrigionalColor;
-                door.GetComponent<SpriteRenderer>().color = OrigionalDoorColor;
-            }
-            else
+            if (timer >= 0)
             {
                 timer -= Time.deltaTime;
                 GetComponent<BoxCollider2D> ().enabled = false;
                 mySR.color = new Color(OrigionalColor.r - 0.5f, OrigionalColor.g - 0.5f, OrigionalColor.b - 0.5f, 1f);
                 door.GetComponent<SpriteRenderer>().color = new Color(OrigionalDoorColor.r - 0.1f, OrigionalDoorColor.g - 0.1f, OrigionalDoorColor.b - 0.1f, 1f);
+            }
+            else
+            {
+                buttonActive = false;
+                timer = timerDuration;
+                GetComponent<BoxCollider2D>().enabled = true;
+                mySR.color = OrigionalColor;
+                door.GetComponent<SpriteRenderer>().color = OrigionalDoorColor;
             }
         }
         else if (buttonActive == true)
