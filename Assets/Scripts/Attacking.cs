1ce0b5d3952491c8b8823328c1ead52c93884076
Nick Santos <santos_nick@outlook.com>
2020-11-13 15:01:37 -0800

    Enemy Change

--- Assets/Scripts/Attacking.cs
+++ Assets/Scripts/Attacking.cs
@@ -22,13 +22,10 @@ public class Attacking : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.J))
         {
-            print("hey");
             if (Player.GetComponent<PlayerController>().facingright == true)
             {
-                print("hi");
                 if (Player.transform.position.x < transform.position.x && Mathf.Abs(Player.transform.position.x - transform.position.x) < 2)
                 {
-                    print("ho");
                     Destroy(gameObject);
                 }
             }
