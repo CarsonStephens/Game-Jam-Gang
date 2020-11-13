6aec72ef6cc09c1871de7e08f2db7bed1ccdd6c5
Gavin Fifer <gavin.fifer@digipen.edu>
2020-11-13 14:31:52 -0800

    Enemy animations done

--- Assets/Scripts/EnemyTracking.cs
+++ Assets/Scripts/EnemyTracking.cs
@@ -16,10 +16,14 @@ public class EnemyTracking : MonoBehaviour
     private bool LookingRight = true;
     Rigidbody2D myRb;
 
+    //this is for animations (By Gavin Fifer)
+    public Animator animator;
+    private SpriteRenderer mySpriteRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -29,11 +33,23 @@ public class EnemyTracking : MonoBehaviour
         {
             LookingRight = true;
             transform.Translate(2 * Time.deltaTime * Speed, 0, 0);
+
+            //Animation Stuff (Gavin Fifer)
+            animator.SetFloat("Speed", 1);
+            mySpriteRenderer.flipX = false;
         }
         else if (Target.transform.position.x < transform.position.x && Target.transform.position.x + 5 > transform.position.x)
         {
             LookingRight = false;
             transform.Translate(-2 * Time.deltaTime * Speed, 0, 0);
+
+            //Animation Stuff (Gavin Fifer)
+            animator.SetFloat("Speed", 1);
+            mySpriteRenderer.flipX = true;
+        }
+        else
+        {
+            animator.SetFloat("Speed", 0);
         }
     }
 }
